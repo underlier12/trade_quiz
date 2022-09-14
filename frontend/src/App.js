37f518e1da442e2a962b91927b1ff8778b172c22
wy.choi <underlier12@gmail.com>
2022-09-14 23:07:51 +0900

    feat: Add tap page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,45 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-import React, { useState, useEffect } from 'react'
+import React from 'react'
+import BasicTabs from './component/Basictaps';
 
 function App() {
-  const [data, setData] = useState(0)
+  // const [data, setData] = useState(0)
 
-  useEffect(() => {
-    fetch('http://localhost:5000/', {
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-      }
-    })
-      .then(res => res.json())
-      .then(data => {
-        setData(data.data)
-        console.log(data)
-      })
-      .catch(error => {
-        console.log(error)
-      })
-  }, [])
+  // useEffect(() => {
+  //   fetch('http://localhost:5000/', {
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //       'Accept': 'application/json'
+  //     }
+  //   })
+  //     .then(res => res.json())
+  //     .then(data => {
+  //       setData(data.data)
+  //       console.log(data)
+  //     })
+  //     .catch(error => {
+  //       console.log(error)
+  //     })
+  // }, [])
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>data is {data}</p>
-      </header>
-    </div>
+    <BasicTabs/>
   );
 }
 

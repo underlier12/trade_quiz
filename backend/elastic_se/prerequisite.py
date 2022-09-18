f21fe9d00d6893321242b198bdcd04ae22a1f753
wy.choi <underlier12@gmail.com>
2022-09-19 00:15:10 +0900

    feat: Add international payment index as prerequisite

--- backend/elastic_se/prerequisite.py
+++ backend/elastic_se/prerequisite.py
@@ -14,3 +14,14 @@ es.indices.create(
     ignore=400,
     body=mapping
 )
+
+# INTERNATIONAL_PAYMENT_MAPPING_PATH = 'elastic_se/trade_english_mapping.json'
+# with open(INTERNATIONAL_PAYMENT_MAPPING_PATH) as f:
+#     mapping = json.load(f)
+
+INDEX = 'international_payment'
+es.indices.create(
+    index=INDEX,
+    ignore=400,
+    body=mapping
+)
